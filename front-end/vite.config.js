b6ffd981cd4c9a13eef579f7110f19793ad8b69d
Bekam Berhanu <Bebabekam@gmail.com>
2026-04-02 01:08:56 +0300

    defining global css

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwind from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer],
+    },
+  },
+});
