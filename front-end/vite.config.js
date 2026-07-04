1760c19db60e0234c44f502dc4c584867c8b1be2
Bekam Berhanu <Bebabekam@gmail.com>
2026-07-05 00:13:31 +0300

    feat: deployment

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -30,7 +30,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 const __filename = fileURLToPath(import.meta.url);
