c0d1ac549707debea0e0e3bb6bc7e7632c098d7b
Bekam Berhanu <Bebabekam@gmail.com>
2026-07-05 00:20:34 +0300

    feat: deployment

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,36 +1,7 @@
-
-// import path from "path"; 
-// import { fileURLToPath } from "url"; 
-// import { defineConfig } from "vite";
-// import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-// import babel from "@rolldown/plugin-babel";
-// import tailwind from "@tailwindcss/postcss";
-// import autoprefixer from "autoprefixer";
-
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-//   css: {
-//     postcss: {
-//       plugins: [tailwind, autoprefixer],
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
-
 import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,19 +9,9 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react()],
 
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-
-  build: {
-    outDir: "dist",
   },
 });
